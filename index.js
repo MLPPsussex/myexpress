1151c2fcf2cb5b3922e31037296e57c464af47b6
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-06-04 12:47:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ app.get('/api/data', (req, res) => {
     count++
     const data = { message: 'Hello!'+count };
 
-    setTimeout( ()=>res.json(data), 100*count);
+    setTimeout( ()=>res.json(data), 100);
 });
+
+app.post('/api/data', (req, res) => {
+  var leoApiKey='693a5c60-f266-4bf7-934f-3db55cd5c5f6'
+  const option1 = {
+  method: 'POST',
+  headers: {
+    accept: 'application/json',
+    'content-type': 'application/json',
+    authorization: `Bearer ${leoApiKey}`
+  },
+  body: JSON.stringify({prompt: 'dog'} ),
+  modelId:"b820ea11-02bf-4652-97ae-9ac0cc00593d", width: dims, height: dims,
+  scheduler: "LEONARDO", num_inference_steps: 30, sd_version: 'v2'})
+};
+leo=""
+fetch('https://cloud.leonardo.ai/api/rest/v1/generations', option1)
+  .then(response => response.json())
+  .then(data => {leo=data.sdGenerationJob.generationId
+                res.json( {id: leo}) }
+       )
+  .catch(err => {res.json( {id: 'APIerror'}) } );
+     
+}
+
 
 // Start the server
 const port = 3000;
